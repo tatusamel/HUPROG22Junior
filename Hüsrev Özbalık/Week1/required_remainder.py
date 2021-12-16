a39008adcf7eade7e5be8eadff51d53f0c435236
Hüsrev <70169764+KhosrovOzbalik@users.noreply.github.com>
2021-12-16 18:05:12 +0300

    Create required_remainder.py

---
+++ Hüsrev Özbalık/Week1/required_remainder.py
@@ -0,0 +1,10 @@
+// Kod çalışıyo site bozuk
+def ultimateFinder(divider, remainder, ultimate):
+    ud = ultimate % divider
+    return ultimate - (ud - remainder if remainder <= ud else divider - (remainder - ud))
+cases = int(input("cases:"))
+final = ""
+for i in range(cases):
+    x,y,n = tuple(map(int,input().split(" ")))
+    final += str(ultimateFinder(x,y,n)) + "\n"
+print(final)
