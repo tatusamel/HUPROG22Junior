169a21947a4584e26094efb77308ade340d1e3d4
Hüsrev <70169764+KhosrovOzbalik@users.noreply.github.com>
2021-12-23 12:58:25 +0300

    Create remove-all-adjacent-duplicates-in-string.java

---
+++ Hüsrev Özbalık/Week2/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        MyBeautifulCharStack stack = new MyBeautifulCharStack();
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) stack.push(s.charAt(i));
+            else if (s.charAt(i) == stack.top()) stack.pop();
+            else stack.push(s.charAt(i));
+        }
+        return stack.toString();
+    }
+}
+
+
+class MyBeautifulCharStack {
+    List<Character> array;
+
+    public MyBeautifulCharStack() {
+        array = new ArrayList<>();
+    }
+
+
+    public boolean isEmpty() {return array.isEmpty(); }
+
+
+    public void push(char var) {array.add(var);}
+    public char top() {
+        if (isEmpty()) throw new EmptyStackException();
+        else return array.get(array.size() - 1);
+    }
+    public char pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        else {
+            char temp = array.get(array.size() - 1);
+            array.remove(array.size() - 1);
+            return temp;
+        }
+    }
+    
+    
+    @Override
+    public String toString() {
+        StringBuilder bobTheBuilder = new StringBuilder();
+        for (char i : array) {
+            bobTheBuilder.append(i);
+        }
+        return bobTheBuilder.toString();
+    }
+}
