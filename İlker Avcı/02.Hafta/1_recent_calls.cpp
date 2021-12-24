372a052a7852de1cef7f570df263b6c33012482d
İlker Avcı <ikolomiko@gmail.com>
2021-12-24 15:36:28 +0300

    Added comments for 1st and 2nd problems

--- İlker Avcı/02.Hafta/1_recent_calls.cpp
+++ İlker Avcı/02.Hafta/1_recent_calls.cpp
@@ -9,9 +9,15 @@ class RecentCounter {
   RecentCounter() : q(new MyQueue(10001)) {}
 
   int ping(int t) {
+    // Add the new ping to the queue
     q->enqueue(t);
+
+    // Remove all elements which are older than [the last ping - 3000 ms]
+    // or equally, t-3000
     while (q->front() < t - 3000)
       q->dequeue();
+
+    // All the remaining elements are at most 3000ms older than the last ping
     return q->size();
   }
 };
--- İlker Avcı/02.Hafta/2_adj_duplicates.cpp
+++ İlker Avcı/02.Hafta/2_adj_duplicates.cpp
@@ -7,17 +7,31 @@ class Solution {
  public:
   string removeDuplicates(string s) {
     auto stack = new MyStack();
+    // Looping through the string in reverse because the stack will be holding
+    // the answer and stack is a LIFO structure. I could've just reversed the
+    // answer string but I am too lazy for that
     for (int i = s.length(); i >= 0; i--) {
       char c = s[i];
+
+      // There can't be duplicates if the stack is empty
       if (stack->isEmpty())
         stack->push(c);
-      else {
+      else {  // stack is not empty
+
+        // If the current letter is the same as the latest element of the stack,
+        // they would destroy each other, resulting in not adding the current
+        // letter to the stack and also removing the last element of the stack.
         if (stack->peek() == c)
           stack->pop();
+
+        // If the current letter is different than the latest element in stack
+        // the current letter can be safely added to the stack
         else
           stack->push(c);
       }
     }
+
+    // Pouring all the elements in the stack into a string
     string result = "";
     while (!stack->isEmpty()) {
       result += (char)stack->pop();
@@ -28,6 +42,7 @@ class Solution {
 };
 
 int main() {
+  // Just a sample for testing
   string str = "abbaca";
 
   Solution* s = new Solution();
