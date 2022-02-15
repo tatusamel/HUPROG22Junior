f11dc70e929406c4d63b5b99dec97e713cb53936
bitsima <85079692+bitsima@users.noreply.github.com>
2022-02-15 23:46:42 +0300

    Delete equalize_the_array.py

--- Betül Sema MANAV/equalize_the_array.py
+++
@@ -1,6 +0,0 @@
-def equalizeArray(arr):
-    max_copies = 0
-    for i in range(0,n):
-        if arr.count(arr[i]) >= max_copies:
-            max_copies = arr.count(arr[i])
-    return n - max_copies 
