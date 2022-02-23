47fe4cf672d3b1e2e1882005f5c914b214f8d972
ysnkmr <80060540+ysnkmr@users.noreply.github.com>
2022-02-23 09:47:26 +0300

    Create main.cpp

---
+++ Yasin Kömür/Hafta 3/main.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+
+void MergeTwo(int A[], int l, int m, int h) {
+    int length1 = m - l + 1;
+    int length2 = h - m;
+
+    int list1[length1];
+    int list2[length2];
+
+    int i, j, k;
+    for (i = 0; i < length1; i++)
+    {
+        list1[i] = A[l + i];
+    }
+    for (j = 0; j < length2; j++)
+    {
+        list2[j] = A[m + 1 + j];
+    }
+
+    i = 0, j = 0, k = l;
+    while (i < length1 && j < length2)
+    {
+        if (list1[i] < list2[j])
+        {
+            A[k++] = list1[i++];
+        }
+
+        else {
+            A[k++] = list2[j++];
+        }    
+    }
+
+    for (; i < length1; i++)
+    {
+        A[k++] = list1[i++];
+    }
+
+    for (; j < length2; j++)
+    {
+        A[k++] = list2[j++];
+    }
+}
+
+void MergeSort(int A[], int l, int h) {
+    int m = (l + h) / 2;
+    if (l < h)
+    {
+        MergeSort(A, l, m);
+        MergeSort(A, m + 1, h);
+        MergeTwo(A, l, m, h);
+    }
+}
+
+
+int main() {
+    int A[8] = {12, 5, 3, 8, 1, 34, 14, 4};
+    MergeSort(A, 0, 7);
+
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << A[i] << " ";
+    }
+    std::cout << std::endl;
+}
