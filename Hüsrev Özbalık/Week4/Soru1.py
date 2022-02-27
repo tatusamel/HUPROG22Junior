492f708764eda6de1922de8b420f7b9d6f3599b8
Hüsrev <70169764+KhosrovOzbalik@users.noreply.github.com>
2022-02-27 20:13:51 +0300

    Create Soru1.py

---
+++ Hüsrev Özbalık/Week4/Soru1.py
@@ -0,0 +1,14 @@
+import math
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    flag = True
+    for j in range(2,int(math.sqrt(n) + 1)):
+        if n % j == 0:
+            print(n // j)
+            flag = False
+            break
+    if flag:
+        print(1)
